e7bb3df7c298d8580eb36e4ba7f13c8450448d17
erfanmoshafi <erfan.moshafi2018@gmail.com>
2021-10-19 15:51:00 +0330

    dlete post update

--- ajax/delete_post.php
+++ ajax/delete_post.php
@@ -4,14 +4,16 @@ include "../config/config.php";
 $result=$conn->query("SELECT * FROM post where id='".$_POST['id']."' ");
 while($row=mysqli_fetch_assoc($result)) {
     if($row['user']==$_SESSION['username']){
-
-        #delete comments
-        $delcomments = $conn->query("DELETE FROM comment where post_id='".$row['id']."'");
-
-        #delete cover
-//        unlink("/upload/".$row['cover']);
-
+//delete cover
+        if(!empty($row['cover'])){
+            unlink(IMAGE_POST_DIR.$row['cover']);
+        }
+        //delete post 
         $conn->query("DELETE FROM post where id='".$_POST['id']."' ");
+        //delete like 
+        $conn->query("DELETE FROM like_post where post_id='".$_POST['id']."' ");
+        //delete comment
+        $conn->query("DELETE FROM comment where post_id='".$_POST['id']."' ");
     }
 }
 
